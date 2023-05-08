4283eac229965ace6814e8c04c84ef3b5dd86994
Jean <jeanz1912@hotmail.com>
2023-05-07 23:52:08 -0500

    primeros pasos con node.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+//esta es la parte intermedia de nuestro proyecto
+//en esta parte realizamos los chequeos necesarios
+//por ultimo vamos a la ultima linea de saludo
+//esto tambien esta comentado
+
+//Este va a ser el mensaje que se envia para despedir
+console.log("Adios");
